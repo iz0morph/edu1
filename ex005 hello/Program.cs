2358238e939eb9036ebf39463f7f5fd32f06ae97
Dmitriy Boytsov <hero575@mail.ru>
2022-11-19 14:48:51 +0300

    hello

---
+++ ex005 hello/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользовтеля");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="маша")
+{
+    Console.WriteLine("Ура, Это Маша");
+}
+else
+{
+    Console.Write("Привет");
+    Console.WriteLine(username);
+    }
